14ea35c40761bbfbc00424ca03d3b75115b6d7d7
Alexander Kiskin <estel4ever@gmail.com>
2017-11-26 15:08:13 +1030

    Moved consts to ENV

--- new-customer.js
+++ new-customer.js
@@ -5,34 +5,21 @@ var documentClient = new AWS.DynamoDB.DocumentClient();
 
 module.exports.main = (event, context, callback) => {
 
-    function response(error, result) {
-        const response = {
-        statusCode: 200,
-        headers: {
-            "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-            "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
-        },
-        body: JSON.stringify({
-            ok: error ? false : true,
-            path: path, 
-            filename: filename, 
-            result: result, 
-            errorText: error ? error.message : ''})
-        };
-        callback(null, response);
-    }
-
+    var customerId = uuidv1();
     var now = new Date(); 
     
     var item = {
-        id: uuidv1(),
+        id: customerId,
         added: now.toISOString(),
         balance: 0,
         activated: false,
         mail: undefined
-    };              
-
+    };  
+    
+    console.log(JSON.stringify(event));
+    
     if (event.body !== null) {
+
         var payload = JSON.parse(event.body);
         if (payload.hasOwnProperty("firstname")) { item.firstname = payload.firstname;}
         if (payload.hasOwnProperty("middlename")) { item.middlename = payload.middlename;}
@@ -49,11 +36,63 @@ module.exports.main = (event, context, callback) => {
         return;
     }
 
-
     var params = {
         TableName: process.env.CUSTOMERS_TABLE,
         Item: item
     };
     //console.log('Params to DB:' + JSON.stringify(params));
-    documentClient.put(params, response);
+    documentClient.put(params, cont);
+
+
+    function response(error, result) {
+        const response = {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+            "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+        },
+        body: JSON.stringify({
+            ok: error ? false : true,
+            errorText: error ? error.message : ''})
+        };
+        callback(null, response);
+    }
+
+    function cont(error, result) {
+        if (error) { response(error); return;}
+
+        let ses = new AWS.SES({region: process.env.SES_REGION});
+        
+        let emailParams = {
+            Destination: {
+              BccAddresses: [],
+              CcAddresses: [],
+              ToAddresses: [item.mail]
+            },
+            Message: {
+              Body: {
+                Text: {
+                  Data: `Hi there. Your access code = ${customerId}`,
+                  Charset: "utf-8"
+                }
+              },
+              Subject: {
+                Data: "SES stuff",
+                Charset: "utf-8"
+              }
+            },
+            Source: process.env.EMAIL_FROM_ADDRESS,
+            ReplyToAddresses: [process.env.EMAIL_FROM_ADDRESS]
+        };        
+
+        ses.sendEmail(emailParams, function (error, data) {
+            if (error) { response(error); return;}
+            
+            //console.log(JSON.stringify(data));
+            response(error, data);
+        });
+    }
+
+
+
 };
