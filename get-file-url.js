fc7c12557e4a68e2a495cd858c18f19e7fa779b2
Alexander Kiskin <estel4ever@gmail.com>
2017-11-27 16:05:18 +1030

    Validating customer id

--- get-file-url.js
+++ get-file-url.js
@@ -3,40 +3,7 @@ const AWS = require('aws-sdk');
 var documentClient = new AWS.DynamoDB.DocumentClient();
 
 module.exports.main = (event, context, callback) => {
- 
-  function response(error, result) {
-    if (error) {
-        
-        const response = {
-        statusCode: 200,
-        headers: {
-            "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-            "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
-        },
-        body: JSON.stringify({
-            ok: true,
-            errorText: error.message})
-        };
-        callback(null, response);
-        
-    } else {
-
-        const response = {
-            statusCode: 302,
-            headers: {
-                "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-                "Access-Control-Allow-Credentials" : true, // Required for cookies, authorization headers with HTTPS
-                "Content-type" : result.ContentType,
-                "Content-Length" : result.ContentLength,
-                "Location" : result
-            } 
-        };
-        callback(null, response);
-
-    }
-  }
-
-
+  
   var id;
   if (event.queryStringParameters !== null && event.queryStringParameters !== undefined) { 
     if (event.queryStringParameters.id !== undefined && event.queryStringParameters.id !== null && event.queryStringParameters.id !== "") { 
@@ -56,15 +23,55 @@ module.exports.main = (event, context, callback) => {
     return;
   }
 
-  var s3 = new AWS.S3();
-  
+  //check CUSTOMER_ID (code) validity
   var params = {
-      Bucket: process.env.TEMP_FILES_BUCKET,
-      Key: id,
-      Expires: 600
+    TableName : process.env.CUSTOMERS_TABLE,
+    Key: {
+      id: code
+    },
+    AttributesToGet: ['id','active']
   };
 
-  //s3.getObject(params, response);
-  s3.getSignedUrl('getObject', params, response);
+  documentClient.get(params, function(error, data) {
+    if (error) { response(error); return; }
+    
+    if (data.Item) { generateUrl(); }
+    else { response(new Error('Customer not found')); }
+  });
 
+  function generateUrl() {
+    var s3 = new AWS.S3();
+    var params = {
+        Bucket: process.env.TEMP_FILES_BUCKET,
+        Key: id,
+        Expires: 600
+    };
+    s3.getSignedUrl('getObject', params, response);
+  }
+
+  function response(error, result) {
+    if (error) {
+      const response = {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+            "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+      },
+      body: JSON.stringify({
+          ok: false,
+          errorText: error.message})
+      };
+      callback(null, response);
+    } else {
+      const response = {
+        statusCode: 302,
+        headers: {
+            "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+            "Access-Control-Allow-Credentials" : true, // Required for cookies, authorization headers with HTTPS
+            "Location" : result
+        } 
+      };
+      callback(null, response);
+    }
+  }
 };
